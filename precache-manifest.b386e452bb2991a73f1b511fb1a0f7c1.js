bc010cdd44d3e6263727dbad779073b155159d6b
Adeola Adeleke <apps@simplyswift.com>
2020-05-24 13:41:16 -0500

    Updates

---
+++ precache-manifest.b386e452bb2991a73f1b511fb1a0f7c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa52b01bf25e2443af734ccf8da49a3",
+    "url": "/cpt-frontend-react/index.html"
+  },
+  {
+    "revision": "2f7929f5b35bd858f196",
+    "url": "/cpt-frontend-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0d64ab47a698a836f0e4",
+    "url": "/cpt-frontend-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f7929f5b35bd858f196",
+    "url": "/cpt-frontend-react/static/js/2.ce8d47a2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cpt-frontend-react/static/js/2.ce8d47a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d64ab47a698a836f0e4",
+    "url": "/cpt-frontend-react/static/js/main.1482e836.chunk.js"
+  },
+  {
+    "revision": "41f64f0f41879aa6e969",
+    "url": "/cpt-frontend-react/static/js/runtime-main.dd2af0e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cpt-frontend-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
